2a128af21905d1d210ab55b59265271d52ebf589
Aamir Sultan <as21afy@herts.ac.uk>
2022-04-24 17:14:41 +0100

    Added DbContext

---
+++ AFI.Infrastructure/AFIDbContext.cs
@@ -0,0 +1,24 @@
+﻿using AFI.Domain.Entities.Customer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFI.Infrastructure
+{
+    public class AFIDbContext : DbContext, IAFIDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public AFIDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
---
+++ AFI.Infrastructure/IAFIDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AFI.Domain.Entities.Customer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFI.Infrastructure
+{
+    public interface IAFIDbContext
+    {
+        DbSet<Customer> Customers { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
