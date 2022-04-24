f068bb72b3d7b9878d14e8216b8f5322c0960c74
Aamir Sultan <as21afy@herts.ac.uk>
2022-04-24 19:42:53 +0100

    Made customer email nullable

--- AFI.Domain/Entities/Customer/Customer.cs
+++ AFI.Domain/Entities/Customer/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,6 @@ namespace AFI.Domain.Entities.Customer
         public string LastName { get; set; }
         public string PolicyRefNumber { get; set; }
         public DateTime? DOB { get; set; }
-        public string Email { get; set; }
+        public string? Email { get; set; }
     }
 }
