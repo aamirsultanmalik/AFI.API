4d472cacb1ed048b67b3857da74937766b72b47d
Aamir Sultan <as21afy@herts.ac.uk>
2022-04-24 18:27:50 +0100

    Added  customer controller

---
+++ AFI.API/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using AFI.Application.Services;
+using AFI.Domain.Entities.Customer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AFI.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet("GetAllCustomers")]
+        public IActionResult Get()
+        {
+            return Ok(_customerService.GetAllCustomers());
+        }
+
+        [HttpPost("RegisterCustomer")]
+        public async Task<IActionResult> Insert(Customer customer)
+        {
+            return Ok(await _customerService.RegisterCustomer(customer));
+        }
+    }
+}
--- AFI.API/Program.cs
+++ AFI.API/Program.cs
@@ -1,8 +1,19 @@
+using AFI.Application.Services;
+using AFI.Infrastructure;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<IAFIDbContext, AFIDbContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("AFI"));
+});
+builder.Services.AddTransient<ICustomerService, CustomerService>();
+builder.Services.AddControllers().AddFluentValidation(config => config.RegisterValidatorsFromAssemblyContaining<CustomerValidator>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
